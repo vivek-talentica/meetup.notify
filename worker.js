d962ef54117c752ee19c0193ede513ec05f7d97a
vivek-talentica <vivek.tikar@talentica.com>
2019-03-20 11:31:06 +0530

    Added gitignore and worker file

---
+++ worker.js
@@ -0,0 +1,25 @@
+self.addEventListener("install", event => {
+  console.log("Installing ...");
+});
+
+self.addEventListener("activate", event => {
+  console.log("Activated ...");
+});
+
+self.addEventListener("notificationclose", function(e) {
+  var notification = e.notification;
+  var primaryKey = notification.data.primaryKey;
+  console.log("Closed notification: " + primaryKey);
+});
+
+self.addEventListener("push", function(e) {
+  var options = {
+    body: e.data.text(),
+    vibrate: [300, 100, 300],
+    data: {
+      dateOfArrival: Date.now(),
+      primaryKey: 1,
+    },
+  };
+  e.waitUntil(self.registration.showNotification("Hello world!", options));
+});
