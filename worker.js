eecd2bc13f9589db27358006aef4764c4cfd87cf
vivek-talentica <vivek.tikar@talentica.com>
2019-03-24 11:05:42 +0530

    Added event - onClick notification redirtect to url and float notification

--- worker.js
+++ worker.js
@@ -12,10 +12,21 @@ self.addEventListener("notificationclose", function(e) {
   console.log("Closed notification: " + primaryKey);
 });
 
+self.addEventListener("notificationclick", function(event) {
+  event.notification.close();
+  event.waitUntil(
+    clients.openWindow(
+      "https://www.yammer.com/talentica.com/#/Threads/show?threadId=71690421649408"
+    )
+  );
+});
+
 self.addEventListener("push", function(e) {
   var options = {
     body: e.data.text(),
+    icon: "https://git-scm.com/images/logos/downloads/Git-Icon-1788C.png",
     vibrate: [300, 100, 300],
+    requireInteraction: true,
     data: {
       dateOfArrival: Date.now(),
       primaryKey: 1,
